cb8c01b0df8ff15901a44e54eb99060b039138e3
Keyshawn Butts <kjbappsllc@gmail.com>
2022-05-04 22:40:29 -0400

    added and removed some stuff

--- src/state.rs
+++ src/state.rs
@@ -5,9 +5,6 @@ use cosmwasm_std::Addr;
 use cw_storage_plus::Item;
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
-pub struct State {
-    pub count: i32,
-    pub owner: Addr,
-}
+pub struct Config {}
 
-pub const STATE: Item<State> = Item::new("state");
+pub const CONFIG: Item<Config> = Item::new("config");
